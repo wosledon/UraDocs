391d0b68f3d02db80b4f502b76ae3d992bdabd65
ledon <powertle@outlook.com>
2024-08-05 20:33:46 +0800

    添加 MenuService.cs 并定义基本方法
    添加了 MenuService.cs 文件，并在其中定义了 MenuService 类。该类包含以下异步方法：GetMenuAsync、UpdateUraMenuAsync、DeleteUraMenuAsync、InsertUraMenuAsync 和 GetUraMenuAsync，当前这些方法均抛出 NotImplementedException。还在文件顶部添加了 using UraDocs.Shared; 语句，并声明了 namespace UraDocs.ApiService.Services; 命名空间。

---
+++ src/UraDocs/UraDocs.ApiService/Services/MenuService.cs
@@ -0,0 +1,31 @@
+﻿using UraDocs.Shared;
+
+namespace UraDocs.ApiService.Services;
+
+public class MenuService
+{
+    public async Task<List<UraMenu>> GetMenuAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task UpdateUraMenuAsync()
+    {
+       throw new NotImplementedException();
+    }
+
+    public async Task DeleteUraMenuAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task InsertUraMenuAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<UraMenu> GetUraMenuAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
